6aa52e15be818372a42094a694a2beb385f36bc9
Nate Granatir <nategranatir@gmail.com>
2017-01-11 23:29:40 -0600

    lesson3 update ids

--- Lesson_3/00_Conference_Central/settings.py
+++ Lesson_3/00_Conference_Central/settings.py
@@ -12,5 +12,5 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '736775150079-p9dc7kb8o66u9l1e6rnfj12djgbutakm.apps.googleusercontent.com'
 
