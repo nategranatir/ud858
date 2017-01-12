655c453b80522a41a39ab5b3c8f0d91e42af6584
Nate Granatir <nategranatir@gmail.com>
2017-01-11 23:35:29 -0600

    create key and put

--- Lesson_3/00_Conference_Central/conference.py
+++ Lesson_3/00_Conference_Central/conference.py
@@ -71,13 +71,15 @@ class ConferenceApi(remote.Service):
         #         and import getUserId from it
         # step 2. get user id by calling getUserId(user)
         # step 3. create a new key of kind Profile from the id
+        user_id = getUserId(user)
+        p_key = ndb.Key(Profile, user_id)
 
         # TODO 3
         # get the entity from datastore by using get() on the key
         profile = None
         if not profile:
             profile = Profile(
-                key = None, # TODO 1 step 4. replace with the key from step 3
+                key = p_key, # TODO 1 step 4. replace with the key from step 3
                 displayName = user.nickname(), 
                 mainEmail= user.email(),
                 teeShirtSize = str(TeeShirtSize.NOT_SPECIFIED),
