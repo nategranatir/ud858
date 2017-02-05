ad7d3f996886b1f716b2aa71ec112dca5c97a977
Nate Granatir <nategranatir@gmail.com>
2017-02-04 19:21:08 -0600

    lesson 5 setting ids

--- Lesson_5/00_Conference_Central/settings.py
+++ Lesson_5/00_Conference_Central/settings.py
@@ -12,7 +12,7 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '736775150079-p9dc7kb8o66u9l1e6rnfj12djgbutakm.apps.googleusercontent.com'
 ANDROID_CLIENT_ID = 'replace with Android client ID'
 IOS_CLIENT_ID = 'replace with iOS client ID'
 ANDROID_AUDIENCE = WEB_CLIENT_ID
--- Lesson_5/00_Conference_Central/static/js/app.js
+++ Lesson_5/00_Conference_Central/static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '736775150079-p9dc7kb8o66u9l1e6rnfj12djgbutakm.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
